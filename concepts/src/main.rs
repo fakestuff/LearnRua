ddf47385433006bf4bcd9397d0cd7ebbf36b7289
Jun Gao <gj_1993@qq.com>
2019-11-15 13:55:27 +0800

    finish data type and mutablility tutorial

--- concepts/src/main.rs
+++ concepts/src/main.rs
@@ -11,4 +11,32 @@ fn main() {
     let x = x * 2;
 
     println!("The value of x is: {}", x);
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+    println!("{}",heart_eyed_cat);
+
+    let tup = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    let a = [1, 2, 3, 4, 5];
+
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let a = [3; 5];
 }
