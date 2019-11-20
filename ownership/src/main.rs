26463456a71f9303048b9093cb0fa372dd1b7994
Jun Gao <gj_1993@qq.com>
2019-11-20 21:04:40 +0800

    ownership

---
+++ ownership/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    {
+        let s = "hello";
+        print!("{}",s);
+        let s = String::from("hello");
+
+        let s1 = String::from("hello");
+        let s2 = s1;
+        print!("{}",s1);
+    
+    }
+    
+}
